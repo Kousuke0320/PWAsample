1c0f012c84d54aa0bc0d6bbe7a9c545f68e152a6
田中康介 <tanakakousuke@tanakakousukenoMacBook-Pro.local>
2019-02-10 16:56:44 +0900

    firt commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAws5lL5NYm9H2DgqBIKXVyJQD_2fSNC20",
+    authDomain: "pwasample-be099.firebaseapp.com",
+    databaseURL: "https://pwasample-be099.firebaseio.com",
+    projectId: "pwasample-be099",
+    storageBucket: "pwasample-be099.appspot.com",
+    messagingSenderId: "800689052756"
+  };
+  firebase.initializeApp(config);
 
 new Vue({
   router,
